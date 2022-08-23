5d603792fc6e0db71e9ae650dab6f660c0784abb
mebeid1 <mae117@pitt.edu>
2022-08-23 19:54:12 -0400

    switched from gruvbox to nano-dark

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("db5b906ccc66db25ccd23fc531a213a1afb500d717125d526d8ff67df768f2fc" default))
  '(package-selected-packages '(nano-theme helm-bibtex ssh)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
